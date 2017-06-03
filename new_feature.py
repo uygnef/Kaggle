633980ce54b77700257361300760d09d11989366
GWeuro <gweuro@gmail.com>
2017-06-03 20:13:34 +1000

    age_at_sale

--- new_feature.py
+++ new_feature.py
@@ -9,6 +9,7 @@ train = data['train']
 def rate_preschool(data):
     data['preschool_quota'].fillna(0)
     data['ratio_preschool'] = data['preschool_quota'] / data['school_quota']
+    data['age_at_sale'] = (pd.to_datetime(data['build_year']) - pd.to_datetime(data['timestamp'])) / np.timedelta64(1, 'Y')
 
 def region_average_high(data):
     mean_height = train.groupby('sub_area')['max_floor'].mean()
